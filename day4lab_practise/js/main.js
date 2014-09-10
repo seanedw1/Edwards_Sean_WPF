2dde5faf007d9e8bf4631b929e9391777f0b7bf2
seanedw1 <seanpro@Seans-MacBook-Pro.local>
2014-09-10 19:20:56 -0400

    day 4 lab practise

---
+++ day4lab_practise/js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by seanpro on 9/10/14.
+ */
+
+var avengernames=["ironman","captain america","thor","black widow","hulk","parthoon","make:skyline",];
+
+console.log(avengernames);
+
+//print specific line in array
+
+console.log(avengernames[6]);
+
+//change an item in array
+avengernames[0]="thor";
+
+//print array again by index number
+console.log(avengernames);
+console.log(avengernames[0]);
+
+//print length of an array
+console.log(avengernames.length);
+
+
---
+++ day4lab_practise/js/prompts.js
@@ -0,0 +1,23 @@
+/**
+ * Created by seanpro on 9/10/14.
+ */
+//prompt
+
+var name=prompt("what is your name?");
+
+console.log(name);
+alert(name);
+
+//calculate the area of rectangle
+
+var width=prompt("what is width of your room");
+var height=prompt("what is the height of your room");
+
+var area=width*height;
+console.log("area");
+alert(area);
+
+var areacode= prompt("what is your area code");
+var phonenumber=prompt("what is your phone number without areacode");
+
+string(areacode) + string(phonenumber);
