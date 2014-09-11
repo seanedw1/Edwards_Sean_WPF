a589266252ffcc0474a90fdd0edc8792850a1acf
seanedw1 <seanpro@Seans-MacBook-Pro.local>
2014-09-10 21:44:08 -0400

    prompts done

--- expressions_personal/js/main.js
+++ expressions_personal/js/main.js
@@ -7,14 +7,19 @@
 //expression person page
 
 
-prompt()
+alert("i own a 99 honda civic and yes she is the love of my life");
+var yourcar=prompt("what type of car do you own");
+alert("I bet my honda is nicer than your"+yourcar);
+
+
 var maintenance=["oil change","car wash","tires","windshield wipers"];
 var oilChange=21.99; //price of oil change
 var carWash=23.99;  //car wash price
 var tires=34.99; //new tires price
 var windshieldWipers=25.00; //price for new wipers
 var totalAmountEachMonth=oilChange+carWash+tires+windshieldWipers;
+var carType="I own a 99 honda civic and yes she is the love of my life";
 
 console.log("every month i spend a total of" +totalAmountEachMonth+ "on my car maintenance");
-console.log("my maintenance items includes" +maintenance);
+console.log("my maintenance items include" +maintenance);
 alert();
