b0f6499b53748ccf3d521a7960249707cd104fbd
seanedw1 <seanpro@Seans-MacBook-Pro.local>
2014-09-10 21:26:09 -0400

    still have to do prompts

--- expressions_personal/js/main.js
+++ expressions_personal/js/main.js
@@ -8,12 +8,13 @@
 
 
 prompt()
+var maintenance=["oil change","car wash","tires","windshield wipers"];
 var oilChange=21.99; //price of oil change
 var carWash=23.99;  //car wash price
 var tires=34.99; //new tires price
 var windshieldWipers=25.00; //price for new wipers
 var totalAmountEachMonth=oilChange+carWash+tires+windshieldWipers;
 
-console.log("every month is spend a total of" +totalAmountEachMonth+ "on my car maintenance");
-var maintenance=["oil change","car wash","tires","windshield wipers"];
+console.log("every month i spend a total of" +totalAmountEachMonth+ "on my car maintenance");
+console.log("my maintenance items includes" +maintenance);
 alert();
