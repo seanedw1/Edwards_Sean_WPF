e499e0ec442193415d2fdec13b71a409d7491ef4
seanedw1 <seanpro@Seans-MacBook-Pro.local>
2014-09-10 23:16:39 -0400

    second one almost done

--- expressions_industry/js/main.js
+++ expressions_industry/js/main.js
@@ -3,22 +3,23 @@
 //wpf section 0
 
 alert("calculate the correct amount of space needed content based on css box model");
-
 var marginSize=prompt("how much of a margin is used");
 var paddingSize=prompt("how much padding is used");
-var borderSize=prompt("size of border");
+var borderSize=prompt("how much border size is used");
 var contentsize=prompt("how many px size will be needed for your content space");
-alert("you will need a total of" + pxNeeded + "to have the correct px");
+
+var pxNeeded=(marginSize-paddingSize-borderSize-contentsize);
+
+alert("you will need a total of " + pxNeeded + " to have the correct px");
 
 
-var margin="";
-var border="";
-var padding="set amount of space";
-var content="area needed";
+var margin="first";
+var border="second";
+var padding="third";
+var content="fourth/nested";
 var boxmodel=["margin","border","padding","content"];
 
 
-var pxNeeded=(marginSize-paddingSize-borderSize-contentsize);
 
 console.log("the correct amount of px needed is"+pxNeeded);
 console.log("my array boxmodel includes the parts of the css box model"+ boxmodel);
