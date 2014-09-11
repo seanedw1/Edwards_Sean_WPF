831169e7da77e588da2785da9aeeeb3c53563ee1
seanedw1 <seanpro@Seans-MacBook-Pro.local>
2014-09-10 21:19:18 -0400

    killing this

--- expressions_personal/js/main.js
+++ expressions_personal/js/main.js
@@ -1,3 +1,19 @@
 /**
  * Created by seanpro on 9/10/14.
  */
+//sean edwards
+//9/10/14
+//wpf section 0
+//expression person page
+
+
+prompt()
+var oilChange=21.99; //price of oil change
+var carWash=23.99;  //car wash price
+var tires=34.99; //new tires price
+var windshieldWipers=25.00; //price for new wipers
+var totalAmountEachMonth=oilChange+carWash+tires+windshieldWipers;
+
+console.log("every month is spend a total of" +totalAmountEachMonth+ "on my car maintenance");
+var maintenance=["oil change","car wash","tires","windshield wipers"];
+alert();
