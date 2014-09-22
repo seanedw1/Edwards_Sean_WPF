7f30d707317f3ea393db464fa4cb9fcadee32e89
seanedw1 <seanpro@Seans-MacBook-Pro.local>
2014-09-22 17:56:51 -0400

    practise

--- day9_practise/js/main.js
+++ day9_practise/js/main.js
@@ -1,3 +1,18 @@
-/**
- * Created by seanpro on 9/22/14.
- */
+//random number function
+var min=100;
+var max=220;
+
+ var ranNum=randomizer(min,max);
+console.log(ranNum)
+
+function randomizer(mn,mx){
+    var randomNumber = (Math.random()*(mx-mn)+mn);
+return  randomNumber;
+
+}
+
+for(var i=0;i<5;i++){
+ console.log(randomizer(min,max));
+
+}
+
