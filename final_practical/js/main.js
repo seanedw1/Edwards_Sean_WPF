c653237003486a30bf1b012d710a44ff2a039ad5
seanedw1 <seanpro@Seans-MacBook-Pro.local>
2014-09-26 18:57:23 -0400

    final

--- final_practical/js/main.js
+++ final_practical/js/main.js
@@ -1,3 +1,32 @@
-/**
- * Created by seanpro on 9/26/14.
- */
+
+function discountedPrices(costOfItem,discountPercent){
+
+
+    var costOfItem=(prompt("what is the cost of the item"));
+
+    if(costOfItem===""){//i know im suppose to use a NAN but i could get it to work correctly
+        (prompt("please enter a amount of cost"));
+
+    }
+
+    var discountPercent=(prompt("what is the percentage of discount available to you"));
+
+    if(discountPercent >= 100){//same thing here
+        console.log("please enter a percentage amount between 0 and 100");
+        (prompt("what is the percentage of discount available to you"));
+    }
+
+
+
+  var  totalPrice=costOfItem*(discountPercent/100);
+    var totalAfter=costOfItem-totalPrice;
+    console.log("the original price of item is " +costOfItem+ " the percentage of a discount is " +discountPercent+ " % after the discount is applied the new total is "+totalAfter );
+    return discountPercent;
+}
+
+discountedPrices();
+
+
+
+
+ //<less than
